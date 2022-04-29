45880cb7e25d566d25feed37f2fe2dfdc25bec6b
Bogdan Mucenica <bogdan@falcontrading.ro>
2022-04-29 11:49:04 +0300

    init

--- README.md
+++ README.md
@@ -10,14 +10,28 @@ composer require falcon/laravel-medialibrary-prune-files
 
 # Run
 
+Basic ussage
+
 ```
-php artisan media-library:prune-files
+php artisan media-library:prune-files app/public
 ```
+
+Checks for media folders inside of a filesystem disk that do not have a media library record in the database. 
+
+A folder is an *"media folder"* if:
+- the folder name is numeric AND
+- the folder contains only files OR the directories within the are either `responsive-images` or `conversions`
 
 If you want to just check what files are going to be deleted, you can use the `--dry-run` command option
 
 ```
-php artisan media-library:prune-files --dry-run
+php artisan media-library:prune-files app/public --dry-run
+```
+
+BY default the package will use the `local` disk. To change it use the `--disk` option 
+
+```
+php artisan media-library:prune-files media-libary --dry-run --disk=public
 ```
 
 # Change log
--- src/Console/Commands/Prune.php
+++ src/Console/Commands/Prune.php
@@ -3,6 +3,11 @@
 namespace Falcon\MediaLibrary\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Symfony\Component\Finder\SplFileInfo;
 
 class Prune extends Command
 {
@@ -11,7 +16,7 @@ class Prune extends Command
      *
      * @var string
      */
-    protected $signature = 'media-library:prune-files {--dry-run}';
+    protected $signature = 'media-library:prune-files {path} {--disk=local} {--dry-run}';
 
     /**
      * The console command description.
@@ -20,6 +25,8 @@ class Prune extends Command
      */
     protected $description = 'Deletes all the files that do not have a Media model record in the DB';
 
+    protected FilesystemAdapter $disk;
+
     /**
      * Execute the console command.
      *
@@ -27,8 +34,81 @@ class Prune extends Command
      */
     public function handle()
     {
-        $files = [];
+        $this->disk = Storage::disk($this->option('disk'));
+        $mediaFolders = $this->getMediaDirectories();
+
+        $toRemove = [];
+        $this->info(__('Computing media list'));
+        $this->withProgressBar($mediaFolders, function(string $path) use (&$toRemove) {
+            if(!Media::where(['id' => basename($path)])->exists()) {
+                $toRemove []= $path;
+            }
+        });
+
+        $this->newLine();
+
+        if($this->option('dry-run')) {
+            $this->table([__('Paths')], collect($toRemove)->map(fn($path) => [$path])->toArray());
+            return 0;
+        }
+
+        $this->info(__('Cleaning up'));
+
+        $this->withProgressBar($toRemove, function(string $path) {
+            $this->disk->deleteDirectory($path);
+        });
+
+        $this->newLine();
+
+        return 0;
+    }
+
+    protected function getMediaDirectories(): array
+    {
+        $mediaDirectories = [];
+        $basePath = $this->argument('path');
+        $directories = $this->disk->allDirectories($basePath);
+
+        foreach ($directories as $directory) {
+            if ($this->isMediaDirectory($directory)) {
+                $mediaDirectories [] = $directory;
+            }
+        }
+
+        return $mediaDirectories;
+    }
+
+    protected function isMediaDirectory(string $path): bool
+    {
+        $reservedDirectoryNames = ['responsive-images', 'conversions'];
+
+        $pathName = basename($path);
+
+        if(!is_numeric($pathName)) {
+            return false;
+        }
+
+        if(in_array($pathName, $reservedDirectoryNames)) {
+            return false;
+        }
+
+        $files = $this->disk->files($path);
+        if(empty($files)) {
+            return false;
+        }
+
+        $directories = $this->disk->directories($path);
+        if(empty($directories)) {
+            return true;
+        }
+
+        foreach($directories as $directory) {
+            $directoryName = basename($directory);
+            if(in_array($directoryName, $reservedDirectoryNames)) {
+                return true;
+            }
+        }
         
-        dd($files);
+        return false;
     }
 }
